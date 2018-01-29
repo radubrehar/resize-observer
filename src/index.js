d4dafee9ce30d5a8db58b87e9abf30439486dd24
radubrehar <radu@jslog.com>
2018-01-29 17:47:43 +0200

    First draft implementation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,63 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { findDOMNode } from 'react-dom';
 
-const ResizeObserver = () => {
-  return <div>resize observer</div>;
+const RENDER = (
+  <div
+    style={{ display: 'none' }}
+    data-name="@rb/resize-observer-placeholder"
+  />
+);
+
+class ReactResizeObserver extends React.Component {
+  componentDidMount() {
+    if (!ResizeObserver) {
+      return;
+    }
+    const node = findDOMNode(this);
+    const target = node.parentNode;
+
+    this.target = target;
+
+    const observer = new ResizeObserver(entries => {
+      this.props.onObserverResize(entries);
+
+      const first = entries[0];
+
+      if (first) {
+        this.props.onResize(first.contentRect, entries);
+      }
+    });
+
+    observer.observe(target);
+
+    this.observer = observer;
+  }
+
+  componentWillUnmount() {
+    delete this.target;
+    if (this.observer) {
+      this.observer.unobserve(target);
+      if (this.observer.disconnect) {
+        this.observer.disconnect();
+      }
+    }
+  }
+
+  render() {
+    return RENDER;
+  }
+}
+
+const emptyFn = () => {};
+
+ReactResizeObserver.defaultProps = {
+  onObserverResize: emptyFn,
+  onResize: emptyFn
+};
+ReactResizeObserver.propTypes = {
+  onObserverResize: PropTypes.func,
+  onResize: PropTypes.func
 };
 
-export default ResizeObserver;
+export default ReactResizeObserver;
